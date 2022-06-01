7f62492ee85818f7a78850eaf466220a257dbf14
Tomasz <ja.tomeasz@gmail.com>
2022-06-01 19:45:00 +0200

    Refactor of simulatedAnnealing

--- src/simulatedAnnealing.cpp
+++ src/simulatedAnnealing.cpp
@@ -9,27 +9,32 @@
 
 void simulatedAnnealing(std::vector<int> startSolution, int binSize, int quantity, int iterations) {
 
-    int score = howManyBin(startSolution, binSize, quantity);
-    int bestScore = score;
+    int score, nextScore;
+    int bestScore = howManyBin(startSolution, binSize, quantity);
     std::vector<int> bestSolution = startSolution;
     std::vector<int> nextSolution = startSolution;
     int iteration_counter = 1;
 
     for (int i = 0; i < iterations; ++i) {
         std::shuffle(std::begin(nextSolution), std::end(nextSolution), std::mt19937(std::random_device()()));
-        if (howManyBin(nextSolution, binSize, quantity) < howManyBin(startSolution, binSize, quantity)) {
+        score = howManyBin(startSolution, binSize, quantity);
+        nextScore = howManyBin(nextSolution, binSize, quantity);
+        if ( nextScore < score ){
             startSolution = nextSolution;
+            score = nextScore;
         } else {
             std::random_device rd;
             std::mt19937 gen(rd());
             std::uniform_real_distribution<> dis(0.0, 1.0);
-            if (dis(gen) < exp(-abs(howManyBin(startSolution, binSize, quantity) - howManyBin(nextSolution, binSize, quantity)) * iteration_counter / 1000)) {
+            if (dis(gen) < exp(-abs(score - nextScore) * iteration_counter / 1000)) {
                 startSolution = nextSolution;
+                score = nextScore;
             }
         }
-        if (howManyBin(startSolution, binSize, quantity) < howManyBin(bestSolution, binSize, quantity)){
+        bestScore = howManyBin(bestSolution, binSize, quantity);
+        if (score < bestScore){
             bestSolution = startSolution;
-            bestScore = howManyBin(bestSolution, binSize, quantity);
+            bestScore = score;
         }
         std::cout << i << " " << score << " " << bestScore << std::endl;
         iteration_counter++;
